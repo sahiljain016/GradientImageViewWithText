121812d9f443e56fd7bde9758c838813483904ee
Sahil Jain <sahiljain332@gmail.com>
2022-07-27 12:23:22 +0530

    Initial commit

---
+++ app/src/main/java/com/auro/sahilkisdk/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.auro.sahilkisdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.auro.sahilkilibrary.GradientImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
